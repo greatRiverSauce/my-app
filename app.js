54eb215fb458023d4a4c9f0089d6d2849856db32
gao <jackiegao2018@hotmail.com>
2018-01-24 23:13:10 -0500

    make it better

--- app.js
+++ app.js
@@ -2,14 +2,29 @@
  * Created by Christine on 2018/1/24.
  */
 var express = require('express'),
-    app = express();
+    app = express(),
+    mongoClient = require('mongodb').MongoClient;
 
 var port = process.env.PORT || 3000;
-
-app.listen(port, function () {
-    console.log("server running @ localhost:3000");
-})
+var connectionUrl = 'mongodb://gao:gaoxinhe@ds113358.mlab.com:13358/gao';
+var mongodb_obj = '';
+mongoClient.connect(connectionUrl, function(err, client) {
+    if (!err) {
+        console.log(mongodb_obj);
+        mongodb_obj = client;
+        app.listen(port, function() {
+            console.log("Server running @ localhost:3000");
+        })
+    }
+});
+// app.listen(port, function () {
+//     console.log("server running @ localhost:3000");
+// })
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
+
+
+
+//mongodb://<dbuser>:<dbpassword>@ds113358.mlab.com:13358/gao
