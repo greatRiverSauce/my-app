610c1885877218cd081770a77c95a128156f2247
gao <jackiegao2018@hotmail.com>
2018-01-24 22:34:05 -0500

    install mongodb

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 /**
  * Created by Christine on 2018/1/24.
  */
+var express = require('express'),
+    app = express();
+
+app.listen(2000, function () {
+    console.log("server running @ localhost:2000");
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
