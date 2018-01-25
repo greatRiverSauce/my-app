73e983463a2d46d6b9c245fd3110b466c0e3811f
gao <jackiegao2018@hotmail.com>
2018-01-24 22:45:43 -0500

    make it better

--- app.js
+++ app.js
@@ -4,8 +4,10 @@
 var express = require('express'),
     app = express();
 
-app.listen(2000, function () {
-    console.log("server running @ localhost:2000");
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log("server running @ localhost:3000");
 })
 
 app.get('/', function (req, res) {
