33d55a7e10c91c827926b499cf062b399d99a815
gao <jackiegao2018@hotmail.com>
2018-01-26 00:46:34 -0500

    one page

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,19 @@
 /**
  * Created by Christine on 2018/1/24.
  */
-var app = angular.module('myapp', ['ngRoute']);
+var app = angular.module('myapp', ["ngRoute"]);
 
 app.config(function($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'view/home.html',
-        resolve:['authService', function (authService) {
-            return authService.isLoggedIn('/');
-        }]
     }).when('/login', {
-        templateUrl:'view/login.html'
+        templateUrl:'view/login.html',
+        controller: 'loginController'
     }).when('/register', {
         templateUrl: 'view/register.html',
         controller: 'registerController'
     }).when('/home', {
         templateUrl:'view/home.html',
-        resolve:['authService', function (authService) {
-            return authService.isLoggedIn('/home');
-        }]
     }).when('/users', {
         templateUrl: 'view/user.html',
         controller: 'userController',
@@ -27,6 +22,7 @@ app.config(function($routeProvider) {
         }]
     });
 });
+
 app.factory('authService', ['$http', '$location', '$rootScope', function ($http, $location, $rootScope) {
         return {
             isLoggedIn: function (path) {
@@ -35,21 +31,11 @@ app.factory('authService', ['$http', '$location', '$rootScope', function ($http,
                 } else {
                     $location.path(path);
                 }
-
-                // $rootScope.$on('$locationChangeStart', function (event, next, current) {
-                //     // redirect to login page if not logged in and trying to access a restricted
-                //     var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
-                //     var loggedIn = $rootScope.globals.currentUser;
-                //     if (restrictedPage && !loggedIn) {
-                //         $location.path('/login');
-                //     }
-                // });
             }
         }
 
 }]);
 app.controller('loginController', ['$location', '$scope', '$http', '$rootScope', function ($location, $scope, $http, $rootScope) {
-
     $scope.login = function () {
         $http.get('http://localhost:3000/getUserByUsername/' + $scope.username)
             .then(function(data) {
@@ -65,19 +51,13 @@ app.controller('loginController', ['$location', '$scope', '$http', '$rootScope',
                 }
             });
     }
-    $scope.register = function () {
-        $location.path('/register');
-    }
 }]);
-app.controller('registerController', ['$location', '$scope', '$http', function ($location, $scope, $http) {
+app.controller('registerController', ['$scope', '$http', function ($scope, $http) {
     $scope.register = function () {
         $http.post('http://localhost:3000/createUser', $scope.user)
             .then(function(data) {
                 alert(data.data.msg);
             });
-    }
-    $scope.login = function () {
-        $location.path('/login');
     }
 }]);
 app.controller('userController', ['$scope', '$http', function ($scope, $http) {
