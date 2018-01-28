2557fcd6f925c313d9a47fb6b2d04a8da1eea927
gao <jackiegao2018@hotmail.com>
2018-01-27 21:20:40 -0500

    finished basice login & sign up

--- public/js/app.js
+++ public/js/app.js
@@ -12,19 +12,35 @@ app.config(function($routeProvider) {
     }).when('/register', {
         templateUrl: 'view/register.html',
         controller: 'registerController'
+    }).when('/logout', {
+        templateUrl: 'view/home.html',
+        controller: 'logoutController'
     }).when('/home', {
         templateUrl:'view/home.html',
     }).when('/profile', {
-        tempplateUrl: 'view/profile.html'
+        templateUrl:'view/profile.html',
+        controller: 'profileController'
+        // resolve:['authService', function (authService) {
+        //     return authService.isLoggedIn('/profile');
+        // }]
     }).when('/setting', {
         templateUrl: 'view/setting.html',
-        controller: 'settingController'
+        controller: 'settingController',
+        resolve:['authService', function (authService) {
+            return authService.isLoggedIn('/setting');
+        }]
     }).when('/setting/editPassword', {
         templateUrl: 'view/userPsd.html',
-        controller: 'userPsdController'
+        controller: 'userPsdController',
+        resolve:['authService', function (authService) {
+            return authService.isLoggedIn('/setting/editPassword');
+        }]
     }).when('/setting/editName', {
         templateUrl: 'view/userName.html',
-        controller: 'userNameController'
+        controller: 'userNameController',
+        resolve:['authService', function (authService) {
+            return authService.isLoggedIn('/setting/editName');
+        }]
     }).when('/users', {
         templateUrl: 'view/user.html',
         controller: 'userController',
@@ -47,31 +63,55 @@ app.factory('authService', ['$http', '$location', '$rootScope', function ($http,
 
 }]);
 
-app.factory('userSettingService', ['$rootScope', '$q', '$http', function ($rootScope, $q, $http) {
+app.factory('userService', ['$rootScope', '$q', '$http', function ($rootScope, $q, $http) {
     var service = {};
     service.Update = Update;
+    service.GetByUsername = GetByUsername;
+    service.Create = Create;
     return service;
+
+    function GetByUsername(name) {
+        var deferred = $q.defer();
+        $http.get('http://localhost:3000/getUserByUsername/' + name)
+            .then(function(data) {
+                deferred.resolve(data.data);
+            });
+        return deferred.promise;
+    }
+
     function Update(user) {
-        //console.log(user);
         var deferred = $q.defer();
         $http.post('http://localhost:3000/updateUser',user)
             .then(function(data) {
-                alert(data.data.flg);
-                //console.log(data.data);
+                deferred.resolve(data.data.flg);
             });
-        deferred.resolve();
-
+        return deferred.promise;
+    }
+    function Create(user) {
+        var deferred = $q.defer();
+        GetByUsername(user.username)
+            .then(function (duplicateUser) {
+                console.log(duplicateUser);
+                if (duplicateUser.length != 0) {
+                    deferred.resolve({success: false, message: 'username "' + user.username + '" is already taken'});
+                } else {
+                    $http.post('http://localhost:3000/createUser', user)
+                        .then(function (data) {
+                            deferred.resolve({success: true, message: data.data.msg});
+                        });
+                }
+            })
         return deferred.promise;
     }
 }])
 
-app.controller('loginController', ['$location', '$scope', '$http', '$rootScope', function ($location, $scope, $http, $rootScope) {
+app.controller('loginController', ['$location', '$scope', '$http', '$rootScope', 'userService', function ($location, $scope, $http, $rootScope, userService) {
     $scope.login = function () {
-        $http.get('http://localhost:3000/getUserByUsername/' + $scope.username)
-            .then(function(data) {
-                if (data.data.length != 0) {
-                    if ($scope.password == data.data[0].password) {
-                        localStorage.setItem('logged', data.data[0].username);
+        userService.GetByUsername($scope.username)
+            .then(function (data) {
+                if (data.length != 0) {
+                    if ($scope.password == data[0].password) {
+                        localStorage.setItem('logged', data[0].username);
                         $location.path('/home');
                     } else {
                         alert("The username/password you entered don't match our record");
@@ -83,16 +123,24 @@ app.controller('loginController', ['$location', '$scope', '$http', '$rootScope',
     }
 }]);
 
-app.controller('registerController', ['$scope', '$http', function ($scope, $http) {
+app.controller('registerController', ['$location', '$scope', '$http', 'userService', function ($location, $scope, $http, userService) {
     $scope.register = function () {
-        $http.post('http://localhost:3000/createUser', $scope.user)
-            .then(function(data) {
-                alert(data.data.msg);
+        userService.Create($scope.user)
+            .then(function (data) {
+                alert(data.message);
+                if (data.success) {
+                    $location.path('/login');
+                }
             });
     }
 }]);
 
-app.controller('settingController', ['$scope', '$http', '$rootScope', 'userSettingService', function ($scope, $http, $rootScope, userSettingService) {
+app.controller('logoutController', ['$location', '$rootScope', function($location, $rootscope) {
+    localStorage.removeItem('logged');
+    $location.path('/home');
+}]);
+
+app.controller('settingController', ['$scope', '$http', '$rootScope', 'userService', function ($scope, $http, $rootScope, userService) {
     $http.get('http://localhost:3000/getUserByUsername/' + localStorage.getItem('logged'))
         .then(function(data) {
             $scope.user = data.data[0];
@@ -108,8 +156,15 @@ app.controller('settingController', ['$scope', '$http', '$rootScope', 'userSetti
 
         });
     $scope.done = function () {
-        userSettingService.Update($scope.user);
+        userService.Update($scope.user)
+            .then(function (data) {
+                alert(data);
+            });
     }
+}])
+
+app.controller('profileController', [function () {
+
 }])
 
 app.controller('userPsdController', ['$scope', '$rootScope', '$http', '$location', function ($scope, $rootScope, $http, $location) {
